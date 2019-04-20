3c139dedac63f86953920d3a9b1ae2db34b4597d
JuYoung <wu7200@naver.com>
2019-04-20 20:01:48 +0900

    SplashLoading
    SplashActivity's Loding Process

--- app/src/main/java/com/example/smartcloset/SplashActivity.java
+++ app/src/main/java/com/example/smartcloset/SplashActivity.java
@@ -3,23 +3,44 @@ package com.example.smartcloset;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
+    ProgressBar mProgBar;
+    Handler handler = new Handler();
+    int value = 0;
+    int add = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        startSplash();
+        mProgBar = findViewById(R.id.progressBar2);
+        startLoading();
     }
-    private void startSplash(){
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+
+    private void startLoading() {
+        Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                overridePendingTransition(0,0);
-                finish();
+                while (true) {
+                    value = value + add;
+                    if (mProgBar.getProgress() == mProgBar.getMax()) {
+                        finish();
+                    }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgBar.setProgress(value);
+                        }
+                    });
+                    try {
+                        Thread.sleep(35);
+                    } catch (InterruptedException e) {
+                    }
+                }
             }
-        }, 2000);
+        });
+        t.start();
     }
 }
